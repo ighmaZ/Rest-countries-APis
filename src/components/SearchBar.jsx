2d96006575f09ab0dffb02d4e93edf61b2c965db
ighmaz <ighmazcool@gmail.com>
2022-10-16 23:24:49 +0530

    build: added search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <>
-    <input type="text" placeholder='search for a country' />
-    
+
+    <input
+     className='w-5/12 text-center shadow-lg border-none text-blue-600'
+     type="text" 
+     placeholder='search for a country' />
     </>
   )
 }
