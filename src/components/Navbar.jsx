27d79c810df7d5033ac75d21db8c42e0631fa8c1
ighmaz <ighmazcool@gmail.com>
2022-10-16 21:11:24 +0530

    buid:navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+
+<nav className='relative w-full flex flex-row items-center justify-between py-8 shadow-lg '>
+
+    <h2  className='text-2xl font-bold ml-3'>Where in the world?</h2>
+    <h2 className='text-2xl font-bold mr-3'>Dark Mode</h2>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
