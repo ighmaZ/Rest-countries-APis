befa8c4447ebe127eba4be823a57a2e1c69f1e5b
ighmaz <ighmazcool@gmail.com>
2022-10-17 22:36:07 +0530

    build:info

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchBar = () => {
-  return (
-    <>
 
+   const[search, setSearch] = useState("");
+
+
+  return (
+ 
+    <>
     <input
-     className='w-5/12 text-center shadow-lg border-none text-blue-600'
+     className='w-5/12 text-center  shadow-lg border-none text-blue-600'
      type="text" 
+     value={search}
+     onChange={(e) => setSearch(e.target.value)}
      placeholder='search for a country' />
     </>
   )
