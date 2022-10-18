7b091a9d378fd76d9732c77f27561f3ae45f0e43
ighmaz <ighmazcool@gmail.com>
2022-10-19 00:34:24 +0530

    build:search fnction

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,7 +24,7 @@ const SearchBar = ({onSearch}) => {
      value={search}
      onChange={(e) => setSearch(e.target.value)}
      placeholder='search for a country' />
-     <button type='submit'>hi</button>
+     <button type='submit' className='ml-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Search</button>
      </form>
     </>
   )
